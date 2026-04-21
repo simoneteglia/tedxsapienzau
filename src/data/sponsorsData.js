25b876aaa5d5b37392c0b46abfb63cfd5953fedb
Simone Teglia <simone.teglia9@gmail.com>
2026-04-21 14:55:41 +0200

    feat: add caiaffa sponsor image and reformat sponsor library structure

--- src/data/sponsorsData.js
+++ src/data/sponsorsData.js
@@ -8,7 +8,10 @@ const sponsorAssetModules = import.meta.glob(
 
 const sponsorAssetMap = Object.fromEntries(
   Object.entries(sponsorAssetModules).map(([path, src]) => [
-    path.split("/").pop().replace(/\.(png|jpe?g|webp|svg)$/i, ""),
+    path
+      .split("/")
+      .pop()
+      .replace(/\.(png|jpe?g|webp|svg)$/i, ""),
     src,
   ]),
 );
@@ -25,79 +28,236 @@ const createSponsorLogo = (key, name, link, options = {}) => ({
 });
 
 const sponsorLogoLibrary = {
-  Autocentri: createSponsorLogo("Autocentri-Balduina", "Autocentri Balduina", "https://www.autocentribalduina.com"),
-  Aesperta: createSponsorLogo("Aesperta", "Aesperta Group", "https://www.aespertagroup.com"),
+  Autocentri: createSponsorLogo(
+    "Autocentri-Balduina",
+    "Autocentri Balduina",
+    "https://www.autocentribalduina.com",
+  ),
+  Aesperta: createSponsorLogo(
+    "Aesperta",
+    "Aesperta Group",
+    "https://www.aespertagroup.com",
+  ),
   AVIS: createSponsorLogo("Avis", "AVIS", "https://www.avisroma.it"),
-  Bonduelle: createSponsorLogo("Bonduelle", "Bonduelle", "https://www.bonduelle.it"),
-  CBCR: createSponsorLogo("CBCR", "Cose Belle Cose Rare", "https://www.icosebellecoserare.com"),
+  Bonduelle: createSponsorLogo(
+    "Bonduelle",
+    "Bonduelle",
+    "https://www.bonduelle.it",
+  ),
+  CBCR: createSponsorLogo(
+    "CBCR",
+    "Cose Belle Cose Rare",
+    "https://www.icosebellecoserare.com",
+  ),
   CE: createSponsorLogo("CE", "CE", "https://www.commission.europa.eu"),
-  CNR: createSponsorLogo("CNR", "Consiglio Nazionale delle Ricerche", "https://www.cnr.it/"),
-  CDP: createSponsorLogo("CDP", "La Consigliera di Parità", "https://www.cittametropolitanaroma.it/homepage/la-citta-metropolitana/struttura-organizzativa/altri-organismi/la-consigliera-di-parita/"),
-  Copernico: createSponsorLogo("Copernico", "Copernico", "https://www.copernicofm.com/"),
+  CNR: createSponsorLogo(
+    "CNR",
+    "Consiglio Nazionale delle Ricerche",
+    "https://www.cnr.it/",
+  ),
+  CDP: createSponsorLogo(
+    "CDP",
+    "La Consigliera di Parità",
+    "https://www.cittametropolitanaroma.it/homepage/la-citta-metropolitana/struttura-organizzativa/altri-organismi/la-consigliera-di-parita/",
+  ),
+  Copernico: createSponsorLogo(
+    "Copernico",
+    "Copernico",
+    "https://www.copernicofm.com/",
+  ),
   CrikCrok: createSponsorLogo("CrikCrok", "Crik Crok", "#"),
-  Deloitte: createSponsorLogo("Deloitte", "Deloitte", "https://www.deloitte.com/it/"),
-  Dias: createSponsorLogo("Dias", "Libreria Dias", "https://www.libreriadias.com/"),
-  DirezioneLavoro: createSponsorLogo("Direzione-Lavoro", "Direzione Lavoro", "https://www.direzionelavoro.it/"),
-  Dotcampus: createSponsorLogo("Dotcampus", "Dotcampus", "https://www.dotcampus.it/"),
+  Deloitte: createSponsorLogo(
+    "Deloitte",
+    "Deloitte",
+    "https://www.deloitte.com/it/",
+  ),
+  Dias: createSponsorLogo(
+    "Dias",
+    "Libreria Dias",
+    "https://www.libreriadias.com/",
+  ),
+  DirezioneLavoro: createSponsorLogo(
+    "Direzione-Lavoro",
+    "Direzione Lavoro",
+    "https://www.direzionelavoro.it/",
+  ),
+  Dotcampus: createSponsorLogo(
+    "Dotcampus",
+    "Dotcampus",
+    "https://www.dotcampus.it/",
+  ),
   eCo: createSponsorLogo("eCo", "eCo", "#"),
-  EF: createSponsorLogo("EF", "Education First", "https://www.ef-italia.it/",{
+  EF: createSponsorLogo("EF", "Education First", "https://www.ef-italia.it/", {
     tileLogoScale: 1.9,
   }),
-  Enav: createSponsorLogo("Enav", "ENAV", "https://www.enav.it/",{
+  Enav: createSponsorLogo("Enav", "ENAV", "https://www.enav.it/", {
     tileLogoScale: 1.9,
     slotLogoScale: 1.68,
   }),
   Enea: createSponsorLogo("Enea", "ENEA", "https://www.enea.it/"),
-  HyperFoundry: createSponsorLogo("Hyper-Foundry", "Hyper Foundry", "https://www.hyper-foundry.com/"),
-  HXO: createSponsorLogo("HXO", "Humanities InventriX Opera", "https://www.hxo.it/"),
-  Innocent: createSponsorLogo("Innocent", "Innocent Drinks", "https://www.innocentdrinks.it/"),
+  HyperFoundry: createSponsorLogo(
+    "Hyper-Foundry",
+    "Hyper Foundry",
+    "https://www.hyper-foundry.com/",
+  ),
+  HXO: createSponsorLogo(
+    "HXO",
+    "Humanities InventriX Opera",
+    "https://www.hxo.it/",
+  ),
+  Innocent: createSponsorLogo(
+    "Innocent",
+    "Innocent Drinks",
+    "https://www.innocentdrinks.it/",
+  ),
   JESAP: createSponsorLogo("JESAP", "JESAP", "https://www.jesap.it/"),
-  Kortpress: createSponsorLogo("Kortpress", "Kortpress", "https://www.kortpress.io/"),
-  Marziali: createSponsorLogo("Marziali", "Marziali", "https://www.marzialicaffe.it/"),
-  MentorsNova: createSponsorLogo("MentorsNova", "Mentors Nova", "https://mentors4u.com/"),
-  MIC: createSponsorLogo("MIC", "Musei in Comune Card", "https://www.museiincomuneroma.it/it/infopage/mic-card/"),
-  Millionaire: createSponsorLogo("Millionaire", "Millionaire", "https://millionaire.it/"),
+  Kortpress: createSponsorLogo(
+    "Kortpress",
+    "Kortpress",
+    "https://www.kortpress.io/",
+  ),
+  Marziali: createSponsorLogo(
+    "Marziali",
+    "Marziali",
+    "https://www.marzialicaffe.it/",
+  ),
+  MentorsNova: createSponsorLogo(
+    "MentorsNova",
+    "Mentors Nova",
+    "https://mentors4u.com/",
+  ),
+  MIC: createSponsorLogo(
+    "MIC",
+    "Musei in Comune Card",
+    "https://www.museiincomuneroma.it/it/infopage/mic-card/",
+  ),
+  Millionaire: createSponsorLogo(
+    "Millionaire",
+    "Millionaire",
+    "https://millionaire.it/",
+  ),
   MyDiet: createSponsorLogo("MyDiet", "MyDiet", "https://mydietroma.it/", {
     tileLogoScale: 1.22,
     slotLogoScale: 1.68,
   }),
-  MSG: createSponsorLogo("MSG", "Ministro per lo Sport e i Giovani", "https://sportegiovani.governo.it/"),
+  MSG: createSponsorLogo(
+    "MSG",
+    "Ministro per lo Sport e i Giovani",
+    "https://sportegiovani.governo.it/",
+  ),
   NAM: createSponsorLogo("NAM", "NAM Studio", "https://namstudio.it/"),
-  NovoNordisk: createSponsorLogo("Novo-Nordisk", "Novo Nordisk", "https://www.novonordisk.it/",{
-    tileLogoScale: 1.3,
-  
-  }),
-  Peekaboo: createSponsorLogo("Peekaboo", "Peekaboo", "https://www.peekaboovision.com/"),
-  Pioda: createSponsorLogo("Pioda", "Pioda Imaging", "https://www.piodaimaging.it/"),
-  PE: createSponsorLogo("PE", "Parlamento Europeo", "https://www.europarl.europa.eu/"),
-  RadioKaos: createSponsorLogo("RadioKaos", "Radio Kaos", "https://www.radiokaositaly.com/"),
-  RadioSapienza: createSponsorLogo("RadioSapienza", "Radio Sapienza", "https://www.radiosapienza.net/"),
-  Redbull: createSponsorLogo("Redbull", "Red Bull", "https://www.redbull.com/it-it/", {
-    tileLogoScale: 1.8,
-  }),
-  RegioneLazio: createSponsorLogo("RegioneLazio", "Regione Lazio", "https://www.regione.lazio.it/"),
-  Roma: createSponsorLogo("Roma", "Comune di Roma Capitale", "https://www.comune.roma.it/"),
-  RFW: createSponsorLogo("RFW", "Rome Future Week", "https://www.romefutureweek.it/"),
+  NovoNordisk: createSponsorLogo(
+    "Novo-Nordisk",
+    "Novo Nordisk",
+    "https://www.novonordisk.it/",
+    {
+      tileLogoScale: 1.3,
+    },
+  ),
+  Peekaboo: createSponsorLogo(
+    "Peekaboo",
+    "Peekaboo",
+    "https://www.peekaboovision.com/",
+  ),
+  Pioda: createSponsorLogo(
+    "Pioda",
+    "Pioda Imaging",
+    "https://www.piodaimaging.it/",
+  ),
+  PE: createSponsorLogo(
+    "PE",
+    "Parlamento Europeo",
+    "https://www.europarl.europa.eu/",
+  ),
+  RadioKaos: createSponsorLogo(
+    "RadioKaos",
+    "Radio Kaos",
+    "https://www.radiokaositaly.com/",
+  ),
+  RadioSapienza: createSponsorLogo(
+    "RadioSapienza",
+    "Radio Sapienza",
+    "https://www.radiosapienza.net/",
+  ),
+  Redbull: createSponsorLogo(
+    "Redbull",
+    "Red Bull",
+    "https://www.redbull.com/it-it/",
+    {
+      tileLogoScale: 1.8,
+    },
+  ),
+  RegioneLazio: createSponsorLogo(
+    "RegioneLazio",
+    "Regione Lazio",
+    "https://www.regione.lazio.it/",
+  ),
+  Roma: createSponsorLogo(
+    "Roma",
+    "Comune di Roma Capitale",
+    "https://www.comune.roma.it/",
+  ),
+  RFW: createSponsorLogo(
+    "RFW",
+    "Rome Future Week",
+    "https://www.romefutureweek.it/",
+  ),
   Sanarti: createSponsorLogo("Sanarti", "Sanarti", "https://www.sanarti.it/"),
-  SapienzaGladiators: createSponsorLogo("SapienzaGladiators", "Sapienza Gladiators", "https://www.sapienzagladiators.it/"),
-  SFClubSapienza: createSponsorLogo("SFClubSapienza", "SF Club Sapienza", "https://www.instagram.com/sfclubsapienza/"),
-  Technotown: createSponsorLogo("Technotown", "TechnoTown", "https:/www.technotown.it/"),
-  TFI: createSponsorLogo("TFI", "Teach For Italy", "https://www.teachforitaly.org/"),
-  Thesis4u: createSponsorLogo("Thesis4u", "Thesis4u", "https://thesisforyou.com/"),
-  Tim: createSponsorLogo("Tim", "TIM", "https://www.gruppotim.it/",{
+  SapienzaGladiators: createSponsorLogo(
+    "SapienzaGladiators",
+    "Sapienza Gladiators",
+    "https://www.sapienzagladiators.it/",
+  ),
+  SFClubSapienza: createSponsorLogo(
+    "SFClubSapienza",
+    "SF Club Sapienza",
+    "https://www.instagram.com/sfclubsapienza/",
+  ),
+  Technotown: createSponsorLogo(
+    "Technotown",
+    "TechnoTown",
+    "https:/www.technotown.it/",
+  ),
+  TFI: createSponsorLogo(
+    "TFI",
+    "Teach For Italy",
+    "https://www.teachforitaly.org/",
+  ),
+  Thesis4u: createSponsorLogo(
+    "Thesis4u",
+    "Thesis4u",
+    "https://thesisforyou.com/",
+  ),
+  Tim: createSponsorLogo("Tim", "TIM", "https://www.gruppotim.it/", {
     tileLogoScale: 1.9,
-
   }),
   Tlon: createSponsorLogo("Tlon", "TLON", "https://tlon.it/"),
   Tucano: createSponsorLogo("Tucano", "Tucano", "https://tucano.com/"),
-  Unicredit: createSponsorLogo("Unicredit", "UniCredit", "https://www.unicredit.it/",{
-    tileLogoScale: 1.9,
-  }),
-  Underpark: createSponsorLogo("UPR", "Underpark Radio", "https://www.underparkradio.com/"),
+  Unicredit: createSponsorLogo(
+    "Unicredit",
+    "UniCredit",
+    "https://www.unicredit.it/",
+    {
+      tileLogoScale: 1.9,
+    },
+  ),
+  Underpark: createSponsorLogo(
+    "UPR",
+    "Underpark Radio",
+    "https://www.underparkradio.com/",
+  ),
   VAIA: createSponsorLogo("VAIA", "VAIA", "https://www.vaia.eu/"),
-  Viscosa: createSponsorLogo("Viscosa", "Collettivo Viscosa", "https://www.instagram.com/collettivoviscosa/"),
+  Viscosa: createSponsorLogo(
+    "Viscosa",
+    "Collettivo Viscosa",
+    "https://www.instagram.com/collettivoviscosa/",
+  ),
   WAYouth: createSponsorLogo("WAYouth", "WA Youth", "https://www.wayouth.it/"),
-  YPT: createSponsorLogo("YPT", "Your Personal Trainer", "https://www.yptrainer.com/"),
+  YPT: createSponsorLogo(
+    "YPT",
+    "Your Personal Trainer",
+    "https://www.yptrainer.com/",
+  ),
   Zeta: createSponsorLogo("Zeta", "Zeta", "#"),
 };
 
@@ -115,8 +275,6 @@ const createLogoSlot = (key, options = {}) => {
 
 const createYearSlot = (year, key, options = {}) =>
   createLogoSlot(key, { year, ...options });
-
-
 
 export const sponsorMarqueeItems = [
   sponsorLogoLibrary.Unicredit,
@@ -162,9 +320,9 @@ export const sponsorSections = [
   {
     id: "mobility-sponsors",
     title: "Mobility Sponsors",
-    slots: [
-      createYearSlot(2026, "Autocentri"),
-    ].filter((slot) => Boolean(slot.src)),
+    slots: [createYearSlot(2026, "Autocentri")].filter((slot) =>
+      Boolean(slot.src),
+    ),
   },
   {
     id: "technical-sponsors",
@@ -180,9 +338,9 @@ export const sponsorSections = [
   {
     id: "supporting-sponsors",
     title: "Supporting Sponsors",
-    slots: [
-      createYearSlot(2026, "Dotcampus"),
-    ].filter((slot) => Boolean(slot.src)),
+    slots: [createYearSlot(2026, "Dotcampus")].filter((slot) =>
+      Boolean(slot.src),
+    ),
   },
   {
     id: "smart-sponsors",
@@ -200,7 +358,6 @@ export const sponsorSections = [
       createYearSlot(2022, "CBCR"),
       createYearSlot(2022, "Deloitte"),
       createYearSlot(2022, "MyDiet"),
-
     ].filter((slot) => Boolean(slot.src)),
   },
   {
@@ -217,6 +374,7 @@ export const sponsorSections = [
     id: "friendly-sponsors",
     title: "Friendly Sponsors",
     slots: [
+      createYearSlot(2026, "DirezioneLavoro"),
       createYearSlot(2025, "EF"),
       createYearSlot(2025, "YPT"),
       createYearSlot(2025, "Redbull"),
@@ -247,7 +405,6 @@ export const sponsorSections = [
       createYearSlot(2022, "Copernico"),
       createYearSlot(2022, "Redbull"),
       createYearSlot(2022, "Marziali"),
-
     ].filter((slot) => Boolean(slot.src)),
   },
   {
@@ -281,29 +438,26 @@ export const sponsorSections = [
       createYearSlot(2022, "eCo"),
       createYearSlot(2022, "RadioKaos"),
       createYearSlot(2022, "Viscosa"),
-
     ].filter((slot) => Boolean(slot.src)),
   },
   {
     id: "technical-partners",
     title: "Technical Partners",
-    slots: [
-      createYearSlot(2026, "Kortpress"),
-    ].filter((slot) => Boolean(slot.src)),
+    slots: [createYearSlot(2026, "Kortpress")].filter((slot) =>
+      Boolean(slot.src),
+    ),
   },
   {
     id: "creative-partners",
     title: "Creative Partners",
-    slots: [
-      createYearSlot(2025, "NAM"),
-      createYearSlot(2024, "NAM"),
-    ].filter((slot) => Boolean(slot.src)),
+    slots: [createYearSlot(2025, "NAM"), createYearSlot(2024, "NAM")].filter(
+      (slot) => Boolean(slot.src),
+    ),
   },
   {
     id: "community-sponsors",
     title: "Community Sponsorss",
     slots: [
-
       createYearSlot(2025, "Thesis4u"),
       createYearSlot(2025, "JESAP"),
       createYearSlot(2025, "SapienzaGladiators"),
@@ -333,10 +487,9 @@ export const sponsorSections = [
   {
     id: "alto-patrocinio",
     title: "Con l'alto patrocinio di",
-    slots: [
-      createYearSlot(2025, "PE"),
-      createYearSlot(2024, "PE"),
-    ].filter((slot) => Boolean(slot.src)),
+    slots: [createYearSlot(2025, "PE"), createYearSlot(2024, "PE")].filter(
+      (slot) => Boolean(slot.src),
+    ),
   },
   {
     id: "patrocinio",
